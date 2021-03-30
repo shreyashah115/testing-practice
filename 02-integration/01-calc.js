8071ef0ca3c10034162a3aa5660732c49d53f746
Shreya <shreyashah115@gmail.com>
2021-03-30 18:06:06 -0400

    Add integration test cases

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,47 +1,89 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
-
+function calc(command, num1, num2) {
+  switch (command) {
+    case "add":
+      var sum = require("../01-unit/01-sum");
+      return sum.action(num1, num2);
+      break;
+    case "subtract":
+      var difference = require("../01-unit/02-difference");
+      return difference.action(num1, num2);
+      break;
+    case "multiply":
+      var product = require("../01-unit/03-product");
+      return product.action(num1, num2);
+      break;
+    case "divide":
+      var quotient = require("../01-unit/04-quotient");
+      return quotient.action(num1, num2);
+      break;
+    default:
+      return "Operation not supported.";
+  }
 }
 
 // ==================================================
 // DEFINE AND EXECUTE TEST CASES
 // ==================================================
 try {
+  // sum(2, 3);
   // --------------------------------------------------
   // Test Case 1
   // --------------------------------------------------
   // It should return the correct sum when the user provides: 'add', 1, 1.
-  var result = calc('add', 1, 1);
-  if (result !== 2) throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
+  var result = calc("add", 1, 1);
+  if (result !== 2)
+    throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
+  var result = calc("subtract", 20, 10);
+  if (result !== 10)
+    throw new Error(
+      'Expected calc("subtract", 20, 10) to be 10. Received: ' + result
+    );
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
+  var result = calc("multiply", 9, 9);
+  if (result !== 81)
+    throw new Error(
+      'Expected calc("multiply", 9, 9) to be 81. Received: ' + result
+    );
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
+  var result = calc("divide", 9, 3);
+  if (result !== 3)
+    throw new Error(
+      'Expected calc("divide", 9, 3) to be 3. Received: ' + result
+    );
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
+  var result = calc("exponent", 2, 8);
+  if (result !== "Operation not supported.")
+    throw new Error(
+      'Expected calc("exponent", 2, 8) to be "Operation not supported.". Received: ' +
+        result
+    );
 
-  console.log('All tests passed successfully.');
+  console.log("All tests passed successfully.");
 
-// ==================================================
-// PRINT ERRORS
-// ==================================================
+  // ==================================================
+  // PRINT ERRORS
+  // ==================================================
 } catch (e) {
-  console.warn('Whoops, the following test did not pass:');
+  console.warn("Whoops, the following test did not pass:");
   console.error(e.message);
 }
